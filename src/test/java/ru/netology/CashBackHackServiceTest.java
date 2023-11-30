9f95a51b87f66db9b48e1f62de4fc7d496e9810a
YuliyaYuliya <yr555556666667777777@gmail.com>
2023-11-30 20:25:30 +0300

    avec GitHub actions

---
+++ src/test/java/ru/netology/CashBackHackServiceTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashBackHackServiceTest {
+
+    @Test
+    void TestCodeWorks() {
+        //1 подготовка данных. 2 выполнение целевого действия. 3. сравнение ожидаемого и фактического результата
+
+        CashBackHackService cashBackHackService = new CashBackHackService(); //1
+        int amount = 900;
+
+        int actual = cashBackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+
+    }
+}

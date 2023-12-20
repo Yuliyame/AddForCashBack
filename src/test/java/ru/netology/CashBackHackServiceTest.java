c6bc0776fdc4cae5f2f9b522d2363685bcb1719b
YuliyaYuliya <yr555556666667777777@gmail.com>
2023-12-20 20:17:05 +0300

    New commit for junit4

--- src/test/java/ru/netology/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/CashBackHackServiceTest.java
@@ -1,20 +1,77 @@
 package ru.netology;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashBackHackServiceTest {
 
-    @Test
+    @org.junit.Test
     public void TestCodeWorks() {
         //1 подготовка данных. 2 выполнение целевого действия. 3. сравнение ожидаемого и фактического результата
 
         CashBackHackService cashBackHackService = new CashBackHackService(); //1
         int amount = 900;
-
         int actual = cashBackHackService.remain(amount);
         int expected = 100;
+
+        assertEquals(expected, actual);
+
+    }
+    @org.junit.Test
+    public void BoundaryTestOne() {
+        //1 подготовка данных. 2 выполнение целевого действия. 3. сравнение ожидаемого и фактического результата
+
+        CashBackHackService cashBackHackService = new CashBackHackService(); //1
+        int amount = 0;
+        int actual = cashBackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+
+    }
+    @org.junit.Test
+    public void BoundaryTestTwo() {
+        //1 подготовка данных. 2 выполнение целевого действия. 3. сравнение ожидаемого и фактического результата
+
+        CashBackHackService cashBackHackService = new CashBackHackService(); //1
+        int amount = 1;
+        int actual = cashBackHackService.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+
+    }
+    @org.junit.Test
+    public void BoundaryTestThree() {
+        //1 подготовка данных. 2 выполнение целевого действия. 3. сравнение ожидаемого и фактического результата
+
+        CashBackHackService cashBackHackService = new CashBackHackService(); //1
+        int amount = 999;
+        int actual = cashBackHackService.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+
+    }
+    @org.junit.Test
+    public void BoundaryTestFour() {
+        //1 подготовка данных. 2 выполнение целевого действия. 3. сравнение ожидаемого и фактического результата
+
+        CashBackHackService cashBackHackService = new CashBackHackService(); //1
+        int amount = 1000;
+        int actual = cashBackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+
+    }
+    @org.junit.Test
+    public void BoundaryTestFive() {
+        //1 подготовка данных. 2 выполнение целевого действия. 3. сравнение ожидаемого и фактического результата
+
+        CashBackHackService cashBackHackService = new CashBackHackService(); //1
+        int amount = 1001;
+        int actual = cashBackHackService.remain(amount);
+        int expected = 999;
 
         assertEquals(expected, actual);
 

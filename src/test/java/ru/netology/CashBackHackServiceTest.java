a7b824988b667d3894970f8b680ca2c4c201d73c
YuliyaYuliya <yr555556666667777777@gmail.com>
2023-12-20 19:40:50 +0300

    boundary tests for testng

--- src/test/java/ru/netology/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/CashBackHackServiceTest.java
@@ -1,5 +1,7 @@
 package ru.netology;
 
+import org.testng.asserts.Assertion;
+
 import static org.testng.Assert.assertEquals;
 
 public class CashBackHackServiceTest {
@@ -17,11 +19,54 @@ public class CashBackHackServiceTest {
 
         assertEquals(actual, expected);
     }
+    @org.testng.annotations.Test
+    public void TestBoundaryValueTwo() {
+        CashBackHackService cashBackHackService = new CashBackHackService(); //1
+        int amount = 0;
+
+        int actual = cashBackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
 
     @org.testng.annotations.Test
-    public void TestSecondTest() {
+    public void TestBoundaryValueThree() {
+        CashBackHackService cashBackHackService = new CashBackHackService(); //1
+        int amount = 1;
+
+        int actual = cashBackHackService.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void TestBoundaryValueFour() {
+        CashBackHackService cashBackHackService = new CashBackHackService(); //1
+        int amount = 999;
+
+        int actual = cashBackHackService.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void TestBoundaryValueFive() {
         CashBackHackService cashBackHackService = new CashBackHackService(); //1
         int amount = 1000;
+
+        int actual = cashBackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void TestBoundaryValueSix() {
+        CashBackHackService cashBackHackService = new CashBackHackService(); //1
+        int amount = 1001;
 
         int actual = cashBackHackService.remain(amount);
         int expected = 0;
